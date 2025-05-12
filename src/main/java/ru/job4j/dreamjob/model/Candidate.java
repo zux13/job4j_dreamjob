b54d71d6a9d048bb9d03a5624297e26622d74d31
Rustem Galiullin <zux.grr@gmail.com>
2025-05-12 10:53:51 +0500

    5. Список кандидатов [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -0,0 +1,68 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Candidate {
+
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime creationDate;
+
+    public Candidate(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.creationDate = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id
+                && Objects.equals(name, candidate.name)
+                && Objects.equals(description, candidate.description)
+                && Objects.equals(creationDate, candidate.creationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, creationDate);
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -0,0 +1,56 @@
+package ru.job4j.dreamjob.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.util.*;
+
+@Repository
+public class MemoryCandidateRepository implements CandidateRepository {
+
+    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
+    private int nextId = 1;
+    private final Map<Integer, Candidate> candidates = new HashMap<>();
+
+    public MemoryCandidateRepository() {
+        save(new Candidate(0, "Alexander", "Junior Java Developer"));
+        save(new Candidate(0, "Maria", "Senior Python Developer"));
+        save(new Candidate(0, "Kristina", "Middle Plus Scala Developer"));
+        save(new Candidate(0, "Roman", "C++ developer"));
+        save(new Candidate(0, "Vyacheslav", "QA Automation Engineer"));
+        save(new Candidate(0, "Alena", "DevOps"));
+    }
+
+    public static MemoryCandidateRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Candidate save(Candidate candidate) {
+        candidate.setId(nextId++);
+        candidates.put(candidate.getId(), candidate);
+        return candidate;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        candidates.remove(id);
+    }
+
+    @Override
+    public boolean update(Candidate candidate) {
+        return candidates.computeIfPresent(candidate.getId(),
+                (id, oldCandidate)
+                        -> new Candidate(oldCandidate.getId(), candidate.getName(), candidate.getDescription())) != null;
+    }
+
+    @Override
+    public Optional<Candidate> findById(int id) {
+        return Optional.ofNullable(candidates.get(id));
+    }
+
+    @Override
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
