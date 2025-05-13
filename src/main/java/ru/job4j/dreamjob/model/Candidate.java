1fcb4c13a7a32c0e1e08e26f627f45625d1d5e78
Rustem Galiullin <zux.grr@gmail.com>
2025-05-13 09:23:28 +0500

    5. Список кандидатов [#504842 #543015]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -55,14 +55,11 @@ public class Candidate {
             return false;
         }
         Candidate candidate = (Candidate) o;
-        return id == candidate.id
-                && Objects.equals(name, candidate.name)
-                && Objects.equals(description, candidate.description)
-                && Objects.equals(creationDate, candidate.creationDate);
+        return id == candidate.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, description, creationDate);
+        return Objects.hash(id);
     }
 }
