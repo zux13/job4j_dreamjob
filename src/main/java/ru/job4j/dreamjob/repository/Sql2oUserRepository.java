c43c0933b9eb46f82108b812f06d6ba8194bf173
Rustem Galiullin <zux.grr@gmail.com>
2025-05-17 10:35:58 +0500

    4. Многопоточность в базе данных [#504860 #543528]

--- src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
@@ -1,7 +1,10 @@
 package ru.job4j.dreamjob.repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 import ru.job4j.dreamjob.model.User;
 
 import java.util.Optional;
@@ -9,6 +12,7 @@ import java.util.Optional;
 @Repository
 public class Sql2oUserRepository implements UserRepository {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Sql2oUserRepository.class);
     private final Sql2o sql2o;
 
     public Sql2oUserRepository(Sql2o sql2o) {
@@ -29,7 +33,8 @@ public class Sql2oUserRepository implements UserRepository {
             int generatedId = query.executeUpdate().getKey(Integer.class);
             user.setId(generatedId);
             return Optional.of(user);
-        } catch (Exception e) {
+        } catch (Sql2oException e) {
+            LOGGER.error("Error saving user: {}", user, e);
             return Optional.empty();
         }
     }
